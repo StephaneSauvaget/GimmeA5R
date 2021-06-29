7ba7785fad293631a94ab7fc651b55218db63c87
arnaud-blanc <mail.github2@blaca.eu>
2021-06-29 12:29:20 +0200

    Order events by date ASC limit 10

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -18,7 +18,8 @@ class UserController extends AbstractController
     {
         $events = $eventRepository->findBy(
             [],
-            ['date' => 'DESC']
+            ['date' => 'ASC'],
+            10
         );
         return $this->render('user/user.html.twig', [
             'events' => $events
