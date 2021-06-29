a2e9ab3a0f33f341ea4ead28ecd677a5e4008f9d
Stéphane Sauvaget <s.sauvaget41000@gmail.com>
2021-06-29 23:28:07 +0200

    reset changes

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,3 @@
-import React from 'react';
-import ReactDOM from "react-dom";
-import Chat from './js/Chat';
+
 import './styles/app.css'
 
-ReactDOM.render(<Chat/>, document.getElementById("root"));
